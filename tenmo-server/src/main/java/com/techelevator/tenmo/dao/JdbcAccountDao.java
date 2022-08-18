fae9d7a0f789089e293fa65e5be96766c4ec4b30
Fazle Rab <fazlerab@gmail.com>
2022-08-18 19:05:44 -0400

    Implemented getBalance

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -0,0 +1,18 @@
+package com.techelevator.tenmo.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class JdbcAccountDao implements AccountDao {
+
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+    @Override
+    public BigDecimal getBalanceByUserId(Long id) {
+        String sql = "SELECT balance FROM account WHERE user_id = ?";
+        return jdbcTemplate.queryForObject(sql, BigDecimal.class, id);
+    }
+}
